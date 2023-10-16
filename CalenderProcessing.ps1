506df07441512bf536d3da36373f5376f59bd536
ach-u-be14 <147650530+ach-u-be14@users.noreply.github.com>
2023-10-16 15:44:52 -0400

    Add files via upload

---
+++ CalenderProcessing.ps1
@@ -0,0 +1,9 @@
+﻿Set-ExecutionPolicy RemoteSigned -Force
+
+Install-Module -Name ExchangeOnlineManagement -Scope CurrentUser
+
+Connect-ExchangeOnline -UserPrincipalName achubet@cmhacc.org
+
+Get-CalendarProcessing -Identity CMHA-233MainStreetBoardRoom715@cmhacc.org | Format-List Identity,DeleteComments,DeleteSubject,AddOrganizerToSubject,RemovePrivateProperty,DeleteAttachments
+
+Set-CalendarProcessing -Identity CMHA-233MainStreetBoardRoom715@cmhacc.org -AddOrganizerToSubject $false -OrganizerInfo $true -DeleteComments $false -DeleteSubject $false -RemovePrivateProperty $false
